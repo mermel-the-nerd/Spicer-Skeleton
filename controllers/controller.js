b784c4fef969aef143f9ee0b0621079c0f171074
mermilmermil <cm26452@westridge.org>
2025-03-10 10:27:59 -0700

    Update controller.js
    added my populateTeachers function

--- controllers/controller.js
+++ controllers/controller.js
@@ -25,3 +25,32 @@ export const getExamples = async (req, res) => {
     }
 };
 
+export const populateTeachers = async (req, res) => {
+    try {
+      const result = [] 
+      const filePath = path.join("./public", 'teachers.csv');
+      fs.readFile(filePath, 'utf8', async (err, data) => {
+        if (err) {
+          res.send("Failed to read the file: \n" + err);
+          
+        }
+        const lines = data.split('\n')
+        const headers = lines[0].split(",") //in case you want to make it more efficient
+        for  (let i = 1; i < lines.length; i++) {
+          const classes = lines[i].split('"')[1].split(",")
+          const nameEmail = lines[i].split(',', 2)
+  
+  
+          const teacher = new Teacher({ name: nameEmail[0], email:  nameEmail[1], classes: classes})
+          await teacher.save()
+          result.push(teacher);
+        }
+        
+        res.send(result)
+      });
+  
+    } catch (err) {
+      console.log(err)
+      res.status(500).send(`Server Error \n ${err}`);
+    }
+  };
