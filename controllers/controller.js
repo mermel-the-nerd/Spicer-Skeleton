9968f09b93bf709ad8cd604256fcb8eb481a8ae8
mermilmermil <cm26452@westridge.org>
2025-03-10 11:18:13 -0700

    update populateTeachers
    made a plan/executed some of my populate Teachers funcitonality

--- controllers/controller.js
+++ controllers/controller.js
@@ -19,7 +19,7 @@ export const loadDataPage = async (req, res) => {
 
 export const populateTeachers = async (req, res) => {
     try {
-        //Course,TEACHER: Email 1,Class ID,Description,Teacher,Block,Grading Periods,Room
+        //Course,email,lastName,firstName,Block,Room
       const result = [] 
       const filePath = path.join("./data", 'data/US Classes (272 records) 2025-03-10.csv'); 
       fs.readFile(filePath, 'utf8', async (err, data) => {
@@ -31,16 +31,26 @@ export const populateTeachers = async (req, res) => {
         const headers = lines[0].split(",") //in case you want to make it more efficient
 				
 				
-
+				let currEmail = ""
         for  (let i = 1; i < lines.length; i++) {
         //   const classes = lines[i].split('"')[1].split(",")
-				// if teacher exists then add to the array 
+				
 				// { name, email, classes: [{name, block, location}]  }
 					
-          const nameEmail = lines[i].split(',')
-  
-  
-          const teacher = new Teacher({ name: nameEmail[0], email:  nameEmail[1], classes: classes})
+          const currLine = lines[i].split(',')
+					if (currEmail === currLine[1]){
+						// if email is the same as last then old model and push new class (do not combine classes)
+					}
+					// if not then create a new teacher
+					else{
+						const teacher = new Teacher({ 
+							name: `${currLine[2]}, ${currLine[3]}`, 
+							email:  currLine[1], 
+							classes: [{name: currLine[0], block: currLine[4], location: currLine[5]}]
+						})
+					}
+					// name = last, first
+          
           await teacher.save()
           result.push(teacher);
         }
